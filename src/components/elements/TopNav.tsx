3c4cfda479d2eb8500b71c530009c0282d9f2048
mengiefen <mengiefen@gmail.com>
2022-08-31 21:42:22 +0300

    add topNav react component

---
+++ src/components/elements/TopNav.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { TbClock } from 'react-icons/tb';
+import { GoLocation } from 'react-icons/go';
+import { AiOutlineShoppingCart, AiOutlinePhone } from 'react-icons/ai';
+
+const TopNav = () => {
+  return (
+    <div className="w-[100vw] fixed top-0 left-0 h-12 bg-slate-900 flex justify-between items-center px-20">
+      <div className="flex gap-5 items-center">
+        <GoLocation className="text-2xl text-yellow-600" />
+        <span className="text-yellow-200">Jimma, Ethiopia </span>
+      </div>
+      <div className="flex gap-5 items-center">
+        <TbClock className="text-2xl text-yellow-600" />
+        <span className="text-yellow-200">Mon-Fri 08:00 AM - 05:00 PM</span>
+      </div>
+      <div className="flex gap-5 items-center">
+        <AiOutlinePhone className="text-2xl text-yellow-600" />
+        <span className="text-yellow-200">(+251) 855-45-56-56</span>
+      </div>
+      <AiOutlineShoppingCart className="text-3xl text-yellow-600" />
+    </div>
+  );
+};
+
+export default TopNav;
