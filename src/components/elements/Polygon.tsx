d2b77c582ed14868ce3ff3d5fe8a0a1984bffc38
mengiefen <mengiefen@gmail.com>
2022-09-04 22:02:19 +0300

    add polygon element

---
+++ src/components/elements/Polygon.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Polygon.module.css';
+
+const Polygon = () => {
+  return (
+    <div
+      className={`${styles.polygon} flex items-center justify-center border-[20px] border-red-700
+       bg-yellow-600 w-[300px] h-[300px] hover:bg-slate-300 shadow-md shadow-yellow-700`}
+    >
+      <h1 className="text-slate-900 text-3xl">MENGSTU</h1>
+    </div>
+  );
+};
+
+export default Polygon;
