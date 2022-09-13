0ab670ad528a778918bcf20dd043f6ae6e092b5b
mengiefen <mengiefen@gmail.com>
2022-09-13 18:06:01 +0300

    add background images

--- src/components/FrontPage/FrontPage.tsx
+++ src/components/FrontPage/FrontPage.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import EnquiryForm from '../elements/EnquiryForm';
-import Image from 'next/image';
-import Container from '../elements/Container';
+import React from "react";
+import EnquiryForm from "../elements/EnquiryForm";
+import Image from "next/image";
+import Container from "../elements/Container";
 
 const FrontPage = () => {
   return (
@@ -9,13 +9,13 @@ const FrontPage = () => {
       <div className="flex flex-col mt-32 max-w-full h-[80%] max-h-screen bg-white-200">
         <div className="h-[100%] flex justify-between items-center ">
           <div className="w-1/2">
-            <h1 className="text-6xl text-slate-90000 font-bold pb-10 ">
-              M & Z Electrical Service
+            <h1 className="text-6xl text-slate-900 font-bold pb-10 ">
+              <span className="text-yellow-700">M & Z</span> Services
             </h1>
             <p className="text-slate-700 text-lg">
               We are a family run electrical service company based in the
               surrounding area. We offer a wide range of services including
-              electrical, plumbing, and electrical and plumbing repairs.
+              electrical, plumbing, and electronics design & selling services.
             </p>
           </div>
           <Image
@@ -23,6 +23,7 @@ const FrontPage = () => {
             width={500}
             height={520}
             alt="electrician"
+            className="hero-image"
           />
         </div>
         <EnquiryForm />
