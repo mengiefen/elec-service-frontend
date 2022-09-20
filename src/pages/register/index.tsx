337b62c9af56c68e0fa6acaece4414a4c5b76462
mengiefen <mengiefen@gmail.com>
2022-09-20 23:04:34 +0300

    include register page

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
 import React from "react";
+import DateTime from "../../components/elements/DateTime";
 
 const register = () => {
   return (
-    <div>
+    <div className="bg-slate-200 min-h-screen ">
       <h1>register</h1>
       <Link href="/">
         <a>Home</a>
       </Link>
+
+      <DateTime />
     </div>
   );
 };
