3b367aa0662f261e5a55ff60fc5f1f2d1878b300
mengiefen <mengiefen@gmail.com>
2022-09-30 00:52:21 +0300

    modify the login page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 import Link from 'next/link';
 import { FaUser, FaFacebookF, FaTwitter, FaGithub } from 'react-icons/fa';
 
@@ -10,27 +10,72 @@ const Login = () => {
     email: '',
     password: '',
   });
+
+  const [move, setMove] = useState({
+    email: '',
+    password: '',
+  });
+
+  const moveLabelToTop = (e: React.FocusEvent<HTMLInputElement>) => {
+    if (e.target.name === 'email' || e.target.value !== '') {
+      setMove({
+        ...move,
+        email: 'move-top',
+      });
+    }
+    if (e.target.name === 'password' || e.target.value !== '') {
+      setMove({
+        ...move,
+        password: 'move-top',
+      });
+    }
+  };
+
+  const moveLabelToBottom = (e: React.FocusEvent<HTMLInputElement>) => {
+    if (e.target.name === 'email' && e.target.value === '') {
+      setMove({
+        ...move,
+        email: '',
+      });
+    }
+
+    if (e.target.name === 'password' && e.target.value === '') {
+      setMove({
+        ...move,
+        password: '',
+      });
+    }
+  };
+
   return (
     <div className="login-card">
-      <div className="py-5 px-3 form">
-        <h2 className="uppercase font-thin text-lg text-center">Welcome to</h2>
-        <h1 className="text-2xl pb-10 text-center">
+      <div className="p-5 form">
+        <h2 className="uppercase font-light text-md text-center">Welcome to</h2>
+        <h1 className="text-2xl pb-3 text-center">
           <span className="block text-3xl font-bold text-yellow-600 ">
             M & Z
           </span>
           Services
         </h1>
+        <p className="pb-5 font-light text-sm text-center">
+          Login to get all our services
+        </p>
+
         <div>
           <form className="login-form">
             <div className="form-group">
+              <label htmlFor="email" className={`form-label ${move.email}`}>
+                Email
+              </label>
               <input
                 type="email"
                 id="email"
                 name="email"
-                placeholder="Email"
-                onChange={(e) =>
-                  setFormData({ ...formData, email: e.target.value })
-                }
+                onChange={(e) => {
+                  setFormData({ ...formData, email: e.target.value });
+                }}
+                onFocus={(e) => moveLabelToTop(e)}
+                onBlur={(e) => moveLabelToBottom(e)}
                 className="form-control"
                 value={formData.email}
               />
@@ -39,17 +84,25 @@ const Login = () => {
             </div>
 
             <div className="form-group">
+              <label
+                htmlFor="password"
+                className={`form-label ${move.password}`}
+              >
+                Password
+              </label>
               <input
                 type="password"
                 id="password"
                 name="password"
-                placeholder="Password"
-                onChange={(e) =>
-                  setFormData({ ...formData, password: e.target.value })
-                }
+                onChange={(e) => {
+                  setFormData({ ...formData, password: e.target.value });
+                }}
+                onFocus={(e) => moveLabelToTop(e)}
+                onBlur={(e) => moveLabelToBottom(e)}
                 className="form-control"
                 value={formData.password}
               />
+
               <BsLockFill className="form-icon" />
             </div>
 
@@ -85,7 +138,7 @@ const Login = () => {
           </div>
         </div>
       </div>
-      <div className="login-right relative backdrop">
+      <div className="login-right relative backdrop hidden md:flex">
         <div className="text-center flex flex-col gap-11 h-full w-full items-center justify-center z-10">
           <h1 className="text-4xl capitalize font-thin">
             <span className="block text-5xl font-bold">M & Z</span>
